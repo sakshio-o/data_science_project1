e27b28daca2bbf03b3599e9e630d4a25d523492d
Sakshi Jadhav <sakshi.jadhav.2306@gmail.com>
2026-04-14 21:30:10 +0530

    Delete program2.py

--- program2.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-if a > b:
-    print("Greatest number is:", a)
-elif b > a:
-    print("Greatest number is:", b)
-else:
-    print("Both numbers are equal")
